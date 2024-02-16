5df88f6f703d3536492b68b8d26de6eac40e9765
Rocky <atoyanrobert21@gmail.com>
2024-02-16 16:47:56 +0500

    add

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://192.168.0.129:3000/","https://forum-amber.vercel.app/"]
+        origin: ["http://192.168.0.129:3000/","https://forum-amber.vercel.app/socket.io"]
     }
 });
 
